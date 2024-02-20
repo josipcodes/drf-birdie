961e0c33c25682a72347027a7d926597262540ad
Josip Potnar <j.potnar@gmail.com>
2024-02-20 13:17:13 +0000

    Create Profile model, migrate to database

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -50,7 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
+
+    'profiles'
 ]
 
 MIDDLEWARE = [
---
+++ profiles/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+# following profile was based on drf_api lessons, but has been modified
+class Profile(models.Model):
+    """
+    Profile model, related to User model
+    """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    name = models.CharField(max_length=150, blank=True)
+    # setting date on creation
+    created = models.DateTimeField(auto_now_add=True)
+    # setting date whenever modified
+    modified = models.DateTimeField(auto_now=True)
+    avatar = models.ImageField(
+        upload_to='images/', default='../default_avatar_x7nup7'
+    )
+
+    # ordering
+    class Meta:
+        ordering = ['-created']
+
+    # returning a human-readable form
+    def __str__(self):
+        return self.owner
+
+
+# based on https://docs.djangoproject.com/en/3.2/ref/signals/
+def create_profile(sender, instance, created, **kwargs):
+    """
+    If created evaluates to True, creates profile
+    """
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
