092ebeaa0bf3825da66b755130a6ca348846086e
Josip Potnar <j.potnar@gmail.com>
2024-02-25 15:06:40 +0000

    Add posts count to category serializer

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,10 +2,19 @@ from rest_framework import serializers
 from .models import Category
 
 class CategorySerializer(serializers.ModelSerializer):
+    post_count = serializers.SerializerMethodField()
+
+    # below function appropriated from:
+    # https://stackoverflow.com/questions/32443471/
+    # django-show-the-count-of-related-objects-in-admin-list-display
+    def get_post_count(self, obj):
+        return obj.post_set.count()
+
     class Meta:
         model = Category
         fields = [
             'id',
             'name',
-            'description'
+            'description',
+            'post_count'
         ]
