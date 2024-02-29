374d91ff58166d75192898047ac344d03b07f5e6
Josip Potnar <j.potnar@gmail.com>
2024-02-29 10:24:36 +0000

    Comment out import in posts views

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 # status potentially unnecessary
 from rest_framework import status, permissions, generics 
-from rest_framework.response import Response
+# from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 # is post detail needed?
