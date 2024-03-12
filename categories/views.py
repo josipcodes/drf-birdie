5d9e7a3fff0a215977e2a386f0e384e7356baccc
Josip Potnar <j.potnar@gmail.com>
2024-03-12 13:58:05 +0000

    Refactor categories views

--- categories/views.py
+++ categories/views.py
@@ -1,27 +1,22 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics 
 from .models import Category
 from .serializers import CategorySerializer
 
-class CategoryList(APIView):
-    def get(self, request):
+class CategoryList(generics.ListCreateAPIView):
         """
-        List all categories
+        List all categories.
+        Permission already set globally in settings.py.
         """
-        all_categories = Category.objects.all()
-        serializer = CategorySerializer(
-            all_categories,
-            many=True,
-        )
-        return Response(serializer.data)
+        # need further checks as to ensure no brute force can be used
+        queryset = Category.objects.all()
+        serializer_class = CategorySerializer
 
 
-class CategoryDetail(APIView):
-    def get(self, request, pk):
-        try:
-            category = Category.objects.get(pk=pk)
-            serializer = CategorySerializer(category)
-            return Response(serializer.data)
-        except Category.DoesNotExist:
-            raise Http404
+class CategoryDetail(generics.RetrieveAPIView):
+    """
+    Retrieve a category.
+    Only Admin is able to amend categories in admin view.
+    Permission already set globally in settings.py.
+    """
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
