6690e5803437c2c516cd0e18ab5d160c7e8b94c8
Josip Potnar <j.potnar@gmail.com>
2024-03-12 13:10:39 +0000

    Delete outdated code in posts, profiles views, add comments

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,3 @@
-# from django.http import Http404
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
@@ -11,65 +7,17 @@ from rest_framework import generics
 # but have been slightly modified before refactoring
 class ProfileList(generics.ListAPIView):
     """
-    List all profiles/create profile
+    List all profiles/create profile.
+    Permission already set globally in settings.py
     """
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
-    # def get (self, request):
-    #     all_profiles = Profile.objects.all()
-    #     serializer = ProfileSerializer(
-    #         all_profiles, 
-    #         many=True,
-    #         context={'request': request}
-    #         )
-    #     return Response(serializer.data)
-
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve a profile. Edit if it is yours.
+    """
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
-    # def get_object(self, pk):
-    #     """
-    #     Function returns a profile or 404
-    #     """
-    #     try:
-    #         profile = Profile.objects.get(pk=pk)
-    #         # based off of https://www.django-rest-framework.org/api-guide/permissions/
-    #         self.check_object_permissions(self.request, profile)
-    #         return profile
-    #     # signal specific profile does not exist
-    #     except Profile.DoesNotExist:
-    #         raise Http404
-
-    # following two methods are copied from drf_api lessons
-    # def get(self, request, pk):
-    #     """
-    #     Obtains profile based on primary key
-    #     """
-    #     profile = self.get_object(pk)
-    #     serializer = ProfileSerializer(
-    #         profile,
-    #         context={'request': request}
-    #         )
-    #     return Response(serializer.data)
-
-    # def put(self, request, pk):
-    #     """
-    #     Obtains profile based on primary key
-    #     Updates profile or serves 400
-    #     """
-    #     profile = self.get_object(pk)
-    #     serializer = ProfileSerializer(
-    #         profile, 
-    #         data=request.data,
-    #         context={'request': request}
-    #         )
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(
-    #         serializer.errors, 
-    #         status=status.HTTP_400_BAD_REQUEST
-    #         )
