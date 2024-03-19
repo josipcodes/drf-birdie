5919c96bab9551e8efa1df66bcdc84f96f376758
Josip Potnar <j.potnar@gmail.com>
2024-03-19 15:10:25 +0000

    Comment out React StrictMode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  // </React.StrictMode>,
+  ,document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
