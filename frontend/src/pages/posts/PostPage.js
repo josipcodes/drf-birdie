f8072cbeae0f432699794f80dd0be9a175540416
Josip Potnar <j.potnar@gmail.com>
2024-03-28 09:19:06 +0000

    Add post and postspage to display multiple posts

---
+++ frontend/src/pages/posts/PostPage.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosRequest } from "../../api/axiosDefaults";
+
+import Post from "./Post";
+
+import postStyles from "../../styles/Post.module.css"
+
+// modelled after Moments lessons
+function PostPage() {
+    const { id } = useParams();
+  // setting an initial value as an empty array in useState to make all the future logic compatible with arrays of objects.
+  // this way it doesn't matter if we're getting a single object or array of posts.
+  const [post, setPost] = useState({ results: [] });
+
+
+  useEffect(() => {
+    // handleMount to fetch the post on mount.
+    const handleMount = async () => {
+      try {
+        // we'll eventually be making 2 requests - posts, comments.
+        // destructuring and renaming variable (to post) in place.
+        // it is called renaming with an object key.
+        // Promises.all accepts an array of promises and gets resolved when all promises get resolved, returning an array of data.
+        // If any of the promises fail, Promise.all gets rejected w/an error.
+        const [{ data: post }] = await Promise.all([
+          axiosRequest.get(`/posts/${id}`),
+        ]);
+        console.log("post", post)
+        // using setPost func to update the results array in the state to contain a post
+        setPost({ results: [post] });
+        // clg post to check that this is working
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    // calling handleMount and running the code every time the id in the url changes.
+    handleMount();
+  }, [id]);
+
+  return (
+    <Row>
+      <Col className="py-2" lg={8}>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={`${appStyles.Content} ${postStyles.PostText}`}>
+          Comments placeholder
+        </Container>
+      </Col>
+      <Col lg={4} className="p-lg-1">
+        Popular categories for desktop
+      </Col>
+    </Row>
+  );
+}
+
+
+export default PostPage;
---
+++ frontend/src/pages/posts/PostsPage.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { axiosRequest } from "../../api/axiosDefaults";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Post from "./Post";
+import Asset from "../../components/Asset";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+
+import appStyles from "../../App.module.css";
+
+// modelled after Moments lessons
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  // keeping track if all data has loaded
+  const [isLoaded, setIsLoaded] = useState(false);
+  // we'll have to re-fetch posts again when user clicks between home, feed and liked pages.
+  // to detect location, we'll use useLocation react router hook.
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        // the request string will contain filter parameter, which comes from the filter prop we set in routes.
+        // tells API if we want to see all posts or certain ones.
+        const { data } = await axiosRequest.get(`/posts/?${filter}`);
+        setPosts(data);
+        // setting IsLoaded to true so spinner no longer spins.
+        setIsLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    // we want spinner displayed before we fetch posts.
+    setIsLoaded(false);
+    // we want this called any time filter or url changes.
+    fetchPosts();
+  }, [filter, pathname]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-lg-1" lg={8}>
+        {/* ternary to display spinner or posts */}
+        {isLoaded ? (
+            // nested ternary to display posts or message.
+            <>
+                {posts.results.length ? (
+                    // map over posts and render each one
+                    posts.results.map((post) => (
+                        // setPosts is needed to like posts.
+                        <Post key={post.id} {...post} setPosts={setPosts} />
+                      ))
+                ) : (
+                    // show no results asset
+                    <Container className={appStyles.Content}>
+                        {/* no results image, message we passed down */}
+                        <Asset message={message} />
+                    </Container>
+                )}
+            </>
+        ) : (
+            // spinner
+            <Container className={appStyles.Content}>
+                <Asset spinner />
+            </Container>
+        )}
+
+      </Col>
+      {/* small screen hook needed, tbd */}
+      <Col lg={4} className="d-none d-lg-block p-lg-1">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default PostsPage;
