09e7438c12c6f5fcd281522ca02e4f30c3b112c2
Josip Potnar <j.potnar@gmail.com>
2024-04-01 13:25:35 +0000

    Clean up category views, enable ordering

--- categories/views.py
+++ categories/views.py
@@ -9,17 +9,16 @@ class CategoryList(generics.ListCreateAPIView):
         Permission already set globally in settings.py.
         """
         # need further checks as to ensure no brute force can be used
-        # queryset = Category.objects.all()
         queryset = Category.objects.annotate(
         posts_count=Count('post', distinct=True),
     ).order_by('-posts_count')
     #     # tbd if necessary here
-    #     filter_backends = [
-    #     filters.OrderingFilter
-    # ]
-    #     ordering_fields = [
-    #     'posts_count',
-    # ]
+        filter_backends = [
+        filters.OrderingFilter
+    ]
+        ordering_fields = [
+        'posts_count',
+    ]
         serializer_class = CategorySerializer
 
 
@@ -30,7 +29,6 @@ class CategoryDetail(generics.RetrieveAPIView):
     Permission already set globally in settings.py.
     """
     serializer_class = CategorySerializer
-    # queryset = Category.objects.all()
     queryset = Category.objects.annotate(
         posts_count=Count('post', distinct=True),
     ).order_by('-posts_count')
