a47014af3bf0d8bf2a89084b58b657ccd252e7ae
Josip Potnar <j.potnar@gmail.com>
2024-04-05 17:53:47 +0000

    Add edit username form, fix styling in password edit

--- frontend/src/pages/profiles/EditUsernameForm.js
+++ frontend/src/pages/profiles/EditUsernameForm.js
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from "react";
 
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 
 import { useHistory, useParams } from "react-router-dom";
 import { axiosResponse } from "../../api/axiosDefaults";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContext";
 
 import bttnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
@@ -18,30 +21,21 @@ import authStyles from "../../styles/AuthForm.module.css";
 
 import bird from "../../assets/bird.jpg";
 
-// copied from Moments with minor adjustments
-const UserEditPasswordForm = () => {
-  const history = useHistory();
-  const { id } = useParams();
-  const currentUser = useCurrentUser();
-
-  const [userData, setUserData] = useState({
-    new_password1: "",
-    new_password2: "",
-  });
-  const { new_password1, new_password2 } = userData;
-
+// based off of Moments with some adjustments
+const EditUsernameForm = () => {
+  const [username, setUsername] = useState("");
   const [errors, setErrors] = useState({});
 
-  const handleChange = (e) => {
-    setUserData({
-      ...userData,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const history = useHistory();
+  const { id } = useParams();
+
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
   useEffect(() => {
-    if (currentUser?.profile_id?.toString() !== id) {
-      // redirect user if they are not the owner of this profile
+    if (currentUser?.profile_id?.toString() === id) {
+      setUsername(currentUser.username);
+    } else {
       history.push("/");
     }
   }, [currentUser, history, id]);
@@ -49,7 +43,13 @@ const UserEditPasswordForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axiosResponse.post("/dj-rest-auth/password/change/", userData);
+      await axiosResponse.put("/dj-rest-auth/user/", {
+        username,
+      });
+      setCurrentUser((prevUser) => ({
+        ...prevUser,
+        username,
+      }));
       history.goBack();
     } catch (err) {
       console.log(err);
@@ -61,36 +61,19 @@ const UserEditPasswordForm = () => {
     <Row className={authStyles.Row}>
       <Col className="my-auto p-md-2" md={5}>
         <Container className={`${appStyles.Content} p-4`}>
-          <h1 className={authStyles.Header}>Change Password</h1>
+          <h1 className={authStyles.Header}>Change Username</h1>
           <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="new_password1">
-              <Form.Label className="d-none">New password</Form.Label>
+            <Form.Group>
+              <Form.Label>Change username</Form.Label>
               <Form.Control
                 className={authStyles.Input}
-                placeholder="New password"
-                type="password"
-                value={new_password1}
-                onChange={handleChange}
-                name="new_password1"
+                placeholder="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
               />
             </Form.Group>
-            {errors?.new_password1?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-            <Form.Group controlId="new_password2">
-              <Form.Label className="d-none">Confirm password</Form.Label>
-              <Form.Control
-                className={authStyles.Input}
-                placeholder="Confirm new password"
-                type="password"
-                value={new_password2}
-                onChange={handleChange}
-                name="new_password2"
-              />
-            </Form.Group>
-            {errors?.new_password2?.map((message, idx) => (
+            {errors?.username?.map((message, idx) => (
               <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
@@ -102,7 +85,7 @@ const UserEditPasswordForm = () => {
               >
                 Cancel
               </Button>
-              <Button type="submit" className={`${bttnStyles.Button}`}>
+              <Button className={`${bttnStyles.Button}`} type="submit">
                 Save
               </Button>
               {errors?.non_field_errors?.map((message, idx) => (
@@ -127,4 +110,4 @@ const UserEditPasswordForm = () => {
   );
 };
 
-export default UserEditPasswordForm;
+export default EditUsernameForm;
