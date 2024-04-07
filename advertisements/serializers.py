ab7a4db3f27d08ebec6300d65eed0b19bd653441
Josip Potnar <j.potnar@gmail.com>
2024-04-07 11:34:37 +0000

    Filter adds on backend

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from .models import Advertisement
 # from django.utils import timezone
-from datetime import date
+# from datetime import date
 
 class AdvertisementSerializer(serializers.ModelSerializer):
-    is_currently_valid = serializers.SerializerMethodField()
+    # is_currently_valid = serializers.SerializerMethodField()
 
-    def get_is_currently_valid(self, obj):
-        today = date.today()
-        return obj.contract_start.date() <= today <= obj.contract_end.date()
+    # def get_is_currently_valid(self, obj):
+    #     today = date.today()
+    #     return obj.contract_start.date() <= today <= obj.contract_end.date()
 
     class Meta:
         model = Advertisement
@@ -21,6 +21,6 @@ class AdvertisementSerializer(serializers.ModelSerializer):
             'money',
             'image',
             'alt',
-            'is_currently_valid'
+            # 'is_currently_valid'
         ]
 
--- advertisements/views.py
+++ advertisements/views.py
@@ -1,15 +1,21 @@
 from rest_framework import permissions, generics 
 from .serializers import AdvertisementSerializer
+# from django_filters.rest_framework import DjangoFilterBackend
 from .models import Advertisement
 from rest_framework.permissions import IsAdminUser
+from datetime import date
 
 class AdvertisementList(generics.ListCreateAPIView):
     """
     View all advertisements
     """
     serializer_class = AdvertisementSerializer
-    queryset = Advertisement.objects.all()
-    # permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        # get only currently valid adds
+        today = date.today()
+        queryset = Advertisement.objects.all().filter(contract_start__lte=today, contract_end__gte=today)
+        return queryset
 
 class AdvertisementDetail(generics.RetrieveAPIView):
     """
